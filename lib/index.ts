18e6c9b54ec03abe1aa3c3741eb779dfff092e5f
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2021-10-19 16:55:37 -0300

    Fixing publication

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,12 @@
-export { DynamoDBAdapter } from './aws/dynamodb/dynamodb-adapter';
-export { DynamoDBConfig } from './aws/dynamodb/dynamodb-config';
-export { SQSAdapter } from './aws/sqs/sqs-adapter';
-export { SSMAdapter } from './aws/ssm/ssm-adapter';
-export { BaseConfigurationOptions } from './aws/base-configuration';
+
+import { DynamoDBAdapter } from './aws/dynamodb/dynamodb-adapter';
+import { DynamoDBConfig } from './aws/dynamodb/dynamodb-config';
+import { SQSAdapter } from './aws/sqs/sqs-adapter';
+import { SSMAdapter } from './aws/ssm/ssm-adapter';
+import { BaseConfigurationOptions } from './aws/base-configuration';
+
+export { DynamoDBAdapter };
+export { DynamoDBConfig }
+export { SQSAdapter }
+export { SSMAdapter }
+export { BaseConfigurationOptions }
