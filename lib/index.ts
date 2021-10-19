2f53f5d01492c3c89880089e7c6fc5ced0b003e1
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2021-10-19 16:31:41 -0300

    Exporting classes

---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+export { DynamoDBAdapter } from './aws/dynamodb/dynamodb-adapter';
+export { DynamoDBConfig } from './aws/dynamodb/dynamodb-config';
+export { SQSAdapter } from './aws/sqs/sqs-adapter';
+export { SSMAdapter } from './aws/ssm/ssm-adapter';
+export { BaseConfigurationOptions } from './aws/base-configuration';
