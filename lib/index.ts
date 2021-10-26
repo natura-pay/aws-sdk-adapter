e589b1b815ae87297ac4f4e8d3266a66839bdfdc
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2021-10-26 15:27:49 -0300

    Fixing exporting error

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,14 @@
 
 import { DynamoDBAdapter } from './aws/dynamodb/dynamodb-adapter';
 import { DynamoDBConfig } from './aws/dynamodb/dynamodb-config';
+
 import { SQSAdapter } from './aws/sqs/sqs-adapter';
+
 import { SSMAdapter } from './aws/ssm/ssm-adapter';
+
 import { SNSAdapter } from './aws/sns/sns-adapter'
 import { SNSConfig } from './aws/sns/sns-config';
+
 import { BaseConfigurationOptions } from './aws/base-configuration';
 
 
