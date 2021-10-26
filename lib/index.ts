705c4f5f623efec55eb14827fa9e5dc3d7a3d91e
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2021-10-26 12:21:11 -0300

    Exporting SNSConfig

--- lib/index.ts
+++ lib/index.ts
@@ -4,6 +4,7 @@ import { DynamoDBConfig } from './aws/dynamodb/dynamodb-config';
 import { SQSAdapter } from './aws/sqs/sqs-adapter';
 import { SSMAdapter } from './aws/ssm/ssm-adapter';
 import { SNSAdapter } from './aws/sns/sns-adapter'
+import { SNSConfig } from './aws/sns/sns-config';
 import { BaseConfigurationOptions } from './aws/base-configuration';
 
 
@@ -12,4 +13,5 @@ export { DynamoDBConfig }
 export { SQSAdapter }
 export { SSMAdapter }
 export { SNSAdapter }
+export { SNSConfig }
 export { BaseConfigurationOptions }
